dd07b4ea67fe6c9256a61934fe099632d3785aee
JJ Bean <jjbean813@outlook.com>
2022-07-16 10:11:26 -0400

    Shortened code down for adding supplies to the .dat file. Now uses a foreach statement.

--- BlueprintMaker/Manager.cs
+++ BlueprintMaker/Manager.cs
@@ -38,35 +38,12 @@ namespace BlueprintMaker
             strings.Add($"Blueprints 1");
             strings.Add($"Blueprint_0_Type {BlueprintType}");
             strings.Add($"Blueprint_0_Supplies {Supplies}");
-            if (SupplyID[0] != 0)
+            int x = 0;
+            foreach (ushort id in SupplyID)
             {
-                strings.Add($"Blueprint_0_Supply_0_ID {SupplyID[0]}");
-                strings.Add($"Blueprint_0_Supply_0_Amount {SupplyAmount[0]}");
-            }
-            if (SupplyID[1] != 0)
-            {
-                strings.Add($"Blueprint_0_Supply_1_ID {SupplyID[1]}");
-                strings.Add($"Blueprint_0_Supply_1_Amount {SupplyAmount[1]}");
-            }
-            if (SupplyID[2] != 0)
-            {
-                strings.Add($"Blueprint_0_Supply_2_ID {SupplyID[2]}");
-                strings.Add($"Blueprint_0_Supply_2_Amount {SupplyAmount[2]}");
-            }
-            if (SupplyID[3] != 0)
-            {
-                strings.Add($"Blueprint_0_Supply_3_ID {SupplyID[3]}");
-                strings.Add($"Blueprint_0_Supply_3_Amount {SupplyAmount[3]}");
-            }
-            if (SupplyID[4] != 0)
-            {
-                strings.Add($"Blueprint_0_Supply_4_ID {SupplyID[4]}");
-                strings.Add($"Blueprint_0_Supply_4_Amount {SupplyAmount[4]}");
-            }
-            if (SupplyID[5] != 0)
-            {
-                strings.Add($"Blueprint_0_Supply_5_ID {SupplyID[5]}");
-                strings.Add($"Blueprint_0_Supply_5_Amount {SupplyAmount[5]}");
+                strings.Add($"Blueprint_0_Supply_{x}_ID {id}");
+                strings.Add($"Blueprint_0_Supply_{x}_Amount {SupplyAmount[x]}");
+                x++;
             }
             if (ToolID != 0)
             {
